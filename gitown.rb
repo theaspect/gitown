bad95e444c413f3fd22916f95015a17391adf41e
Constantine Linnick <theaspect@gmail.com>
2021-12-30 03:04:16 +0700

    Draw both year retention and developer retention

--- gitown.rb
+++ gitown.rb
@@ -4,11 +4,10 @@ require 'json'
 require 'date'
 
 class StatCache
-  attr_reader :commits, :blobs, :authors, :replacements
+  attr_reader :commits, :blobs, :authors, :years, :replacements
 
-  def initialize(path, output, branch, limit, interval, replacements = {}, callback = 'callback', extensions = ['*.*'])
+  def initialize(path, branch, limit, interval, replacements = {}, callback = 'callback', extensions = ['*.*'])
     @path = path
-    @output = output
     @replacements = replacements
     @branch = branch
     @limit = limit
@@ -16,10 +15,11 @@ class StatCache
     @callback = callback
     @extensions = extensions
 
-    @repo = Rugged::Repository.new(path)
+    @repo = Rugged::Repository.new(@path)
 
     # Cached data
     @authors = {}
+    @years = {}
     @commits = {}
     @blobs = {}
   end
@@ -32,16 +32,20 @@ class StatCache
     ).first(@limit)
   end
 
-  def get_commit(hash)
+  # Return author: and year:
+  def get_commit_info(hash)
     unless @commits.key?(hash)
       begin
-        author = @repo.lookup(hash).author[:email]
+        object = @repo.lookup(hash)
+        author = object.author[:email]
+        year = Time.at(object.epoch_time).year
       rescue StandardError => e
         puts hash
         raise e
       end
-      @commits[hash] = author
+      @commits[hash] = { author: author, year: year }
       @authors[get_name(author)] = 0
+      @years[year] = 0
       # else
       # puts "Skip author"
     end
@@ -54,40 +58,10 @@ class StatCache
     rules.any? { |ext| File.fnmatch(ext, name) }
   end
 
-  def annotate_file(commit, root, entry)
-    if !@blobs.key?(entry[:oid]) && validate_entry(@extensions, "#{root}#{entry[:name]}")
-      stats = Hash.new(0)
-      command = "git --no-pager -C #{@path} blame -l -r #{commit.oid} -- '#{root}#{entry[:name]}'"
-      begin
-        output = `#{command}`
-        output.each_line do |r|
-          # looks like bug in git blame
-          # command could return ^24aab5e4254889039a3aa64d810f47d487aefd9 (john 2013-04-17 21:32:40 +0400 1) some line
-          author = get_commit(r[0] != '^' ? r[0, 39] : r[1, 39])
-          stats[get_name(author)] += 1
-        rescue StandardError => e
-          puts r
-          raise e
-        end
-      rescue StandardError => e
-        puts command
-        raise e
-      end
-      @blobs[entry[:oid]] = stats
-      # noinspection RubyUnnecessaryReturnValue
-      @blobs[entry[:oid]]
-    else
-      # puts "Skip blob"
-      Hash.new(0)
-    end
-  end
-
-  def get_name(name)
-    if @replacements.key? name
-      @replacements[name]
-    else
-      name
-    end
+  # In case a committer has multiple emails run through replacements
+  def get_name(email)
+    # TODO: Could be a refactoring problem too, report to JetBrains
+    @replacements.key?(email) ? @replacements[email] : email
   end
 
   def write_totals
@@ -96,103 +70,144 @@ class StatCache
     puts "Authors #{@authors.keys}"
   end
 
-  def write_output(result)
-    write_totals
-
+  def write_output(result, statistics, items, output, smooth)
     # Sort
     sorted_stats = result.sort_by { |k, _v| k }.to_h # {date: stats,...}
     max = 0
     sorted_stats.each do |_k, v|
-      total = v.values.reduce(:+)
+      total = v[statistics].values.reduce(:+)
       max = total if max < total
     end
     # Dates
     dates = sorted_stats.keys
     # Stats per author
-    stats_per_author = []
+    stats_per_bucket = []
     # Iterate over author
-    @authors.each_key do |author|
+    items.each_key do |stat|
       author_stat = []
       dates.each do |date|
-        if sorted_stats[date][author]
-          author_stat.push({ date: date, value: sorted_stats[date][author] })
+        if sorted_stats[date][statistics][stat]
+          author_stat.push({ date: date, value: sorted_stats[date][statistics][stat] })
         else
           author_stat.push({ date: date, value: 0 })
         end
       end
-      stats_per_author.push({ author: author, stats: author_stat })
+      stats_per_bucket.push({ author: stat, stats: author_stat })
     end
 
     begin
-      File.open(@output, 'w') do |f|
+      File.open(output, 'w') do |f|
         f.puts "#{@callback}("
+        f.puts "\"#{statistics}\","
+        f.puts "#{smooth},"
         f.puts "[\"#{dates[0]}\",\"#{dates[-1]}\"],"
         f.puts "[0,#{max}],"
-        f.puts "#{JSON.pretty_generate(@authors.keys)},"
-        f.puts "#{JSON.pretty_generate(stats_per_author)});"
+        f.puts "#{JSON.pretty_generate(items.keys)},"
+        f.puts "#{JSON.pretty_generate(stats_per_bucket)});"
       end
     rescue StandardError => e
-      puts "Cannot write to #{@output}"
+      puts "Cannot write to #{output}"
       raise e
     end
   end
 
-  def map_date(date, interval)
+  def truncate_date(date, interval)
     case interval
     when 'daily'
       date.strftime('%F')
     when 'weekly'
       # First day of every week
+      # %G - The week-based year
+      # %V - Week number of the week-based year (01..53)
+      # %GW%V formatted as 2021W53 then parsed back to dat effectively trim to first day of week
+      # Then format as iso date
+      # %F - The ISO 8601 date format (%Y-%m-%d)
       Date.strptime(date.strftime('%GW%V'), '%GW%V').strftime('%F')
+    when 'monthly'
+      # Same as above
+      # %m - Month of the year, zero-padded (01..12)
+      Date.strptime(date.strftime('%Y%m'), '%Y%m').strftime('%F')
     else
       raise "Unknown interval #{interval}"
     end
   end
 
-  def gather_stats
-    result = {}
+  # File could be formed from several blobs so we caching each blob statistics
+  def annotate_file_blob(commit, root, entry)
+    return @blobs[entry[:oid]] if @blobs.key?(entry[:oid])
 
-    each_commit do |c, _time|
-      # Gather stats once per day
-      time = map_date(c.time, @interval)
-      if result.key? time
-        # puts "Skip #{time}"
-        next
+    author_stats = {}
+    author_stats.default_proc = proc { |hash, key| hash[key] = 0 }
+    year_stats = {}
+    year_stats.default_proc = proc { |hash, key| hash[key] = 0 }
+    command = "git --no-pager -C #{@path} blame -l -r #{commit.oid} -- '#{root}#{entry[:name]}'"
+    begin
+      output = `#{command}`
+      output.each_line do |r|
+        # looks like bug in git blame
+        # command could return ^24aab5e4254889039a3aa64d810f47d487aefd9 (john 2013-04-17 21:32:40 +0400 1) some line
+        author, year = get_commit_info(r[0] != '^' ? r[0, 39] : r[1, 39]).values_at(:author, :year)
+        author_stats[get_name(author)] += 1
+        year_stats[year] += 1
+      rescue StandardError => e
+        puts r
+        raise e
       end
-
-      result[time] = inject_blobs(c) { |r, e, stats| stats.merge!(annotate_file(c, r, e)) { |_k, v1, v2| v1 + v2 } }
+    rescue StandardError => e
+      puts command
+      raise e
     end
-
-    result
+    @blobs[entry[:oid]] = { authors: author_stats, years: year_stats }
+    # TODO: report intellij idea team about broken logic
+    # noinspection RubyUnnecessaryReturnValue
+    @blobs[entry[:oid]]
   end
 
-  def each_commit
-    commits_count = walker.count
-    cc = 0
-    walker.each_entry do |c|
-      # Format time as "2016-12-31"
-      time = c.time.strftime('%F')
-
-      cc += 1
-      print "#{cc}/#{commits_count} for #{time}\r"
-
-      yield c, time
-    end
-  end
-
-  def inject_blobs(commit, stats = Hash.new(0))
+  def fold_blobs(commit, stats = {})
     # files_count = c.tree.count_recursive
     # puts "Files #{files_count}"
-    i = 0
-    commit.tree.walk_blobs(:preorder) do |r, e|
-      i += 1
-      blob = @repo.lookup(e[:oid])
-      unless blob.binary?
-        yield r, e, stats # Each blob
+    # i = 0
+    commit.tree.walk_blobs(:preorder) do |path, entry|
+      # i += 1
+      blob = @repo.lookup(entry[:oid])
+      if !blob.binary? && validate_entry(@extensions, "#{path}#{entry[:name]}")
+        yield path, entry, stats # Each blob
       end
       # puts "#{i} out of #{files_count}" if i % 100 == 0
     end
     stats
+  end
+
+  # Iterate all commits from the first one
+  def each_commit
+    commits_count = walker.count
+    walker.each_with_index do |commit, index|
+      # Format time as "2016-12-31"
+      print "#{index}/#{commits_count} for #{commit.time.strftime('%F')}\r"
+      yield commit
+    end
+  end
+
+  def gather_stats
+    buckets = {}
+
+    each_commit do |commit|
+      # Gather stats once per day
+      time = truncate_date(commit.time, @interval)
+      # If this bucket already exists then skip it
+      if buckets.key? time
+        # puts "Skip #{time}"
+        next
+      end
+
+      buckets[time] = fold_blobs(commit) do |path, entry, stats|
+        stats.merge!(annotate_file_blob(commit, path, entry)) do |_k, v1, v2|
+          v1.merge(v2) { |key, old, new| old + new }
+        end
+      end
+    end
+
+    buckets
   end
 end
 
@@ -219,7 +234,6 @@ end
 
 cache = StatCache.new \
   config.fetch('path'), \
-  config.fetch('output', 'output.json'), \
   config.fetch('branch', 'master'), \
   config.fetch('limit', 1_000_000), \
   config.fetch('interval', 'daily'), \
@@ -228,4 +242,6 @@ cache = StatCache.new \
   config.fetch('extensions', ['*.*'])
 
 result = cache.gather_stats
-cache.write_output result
+cache.write_totals
+cache.write_output result, :authors, cache.authors, config.fetch('authors', 'authors.json'), config.fetch('smooth', 1)
+cache.write_output result, :years, cache.years, config.fetch('years', 'years.json'), config.fetch('smooth', 1)
